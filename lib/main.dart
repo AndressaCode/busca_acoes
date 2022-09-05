b7fe5cf2924de2d76421051fd544cc4871deed9e
Andressa Silva <andressaszx@gmail.com>
2022-09-04 23:20:38 -0300

    add method to fetch stocks

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,10 @@
+
+import 'package:busca_acoes/pages/home.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(
+void main() async{
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Home(),
   ));
-}
-
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Busca de Ações'),
-        centerTitle: true,
-      ),
-    );
-  }
 }
